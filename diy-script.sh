d840f90673acc86c7c602f1bee8381556b78d038
yasuisss <165147567+yasuisss@users.noreply.github.com>
2024-08-19 03:09:07 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -86,6 +86,9 @@ git_sparse_clone master https://github.com/linkease/nas-packages network/service
 git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
 
+# 友善uboot
+sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/s/^/#/' package/boot/uboot-rk35xx/Makefile
+
 # 在线用户
 git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
 sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings

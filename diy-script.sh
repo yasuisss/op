1e74249037bd817b5b1efbd2737bac78f7ac322a
yasuisss <165147567+yasuisss@users.noreply.github.com>
2026-05-13 02:01:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -154,6 +154,9 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
 
+# 测试开启bbr3
+sed -i '/exit 0/i echo bbr3 > /proc/sys/net/ipv4/tcp_congestion_control' /etc/rc.local
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 ./scripts/feeds update luci
